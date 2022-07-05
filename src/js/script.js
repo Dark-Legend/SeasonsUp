7e113c05ef4cea32ccf4cfe64aed804f74455a6f
Mohit Sharma <iamxguy352@gmail.com>
2022-07-06 00:26:11 +0530

    Successfully implemented enter on search functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,72 +1,132 @@
-'use strict'
+"use strict";
 
-const input = document.querySelector('.search');
-const searchBtn = document.querySelector('.search-icon');
-const getDeviceLocation = document.querySelector('.get-current-location');
-const clearBtn = document.querySelector('.clear');
-const quoteContainer = document.querySelector('.container');
-const cardsContainer = document.querySelector('.weather-cards');
-let temp = document.querySelector('.temp');
-let description = document.querySelector('.description');
-let humid = document.querySelector('.humid-temp');
-let cityCountry = document.querySelector('.city-country');
-let minMax = document.querySelector('.min-max-temp');
-const weatherIcon = document.querySelector('.weather-icon')
+const input = document.querySelector(".search");
+const searchBtn = document.querySelector(".search-icon");
+const getDeviceLocation = document.querySelector(".get-current-location");
+const clearBtn = document.querySelector(".clear");
+const quoteContainer = document.querySelector(".container");
+const cardsContainer = document.querySelector(".weather-cards");
+let temp = document.querySelector(".temp");
+let description = document.querySelector(".description");
+let humid = document.querySelector(".humid-temp");
+let cityCountry = document.querySelector(".city-country");
+let minMax = document.querySelector(".min-max-temp");
+const weatherIcon = document.querySelector(".weather-icon");
+
 // ============Clear Button Function===============
 
-clearBtn.addEventListener('click',function(){
-    input.value = '';
-    quoteContainer.classList.remove('hidden');
-    cardsContainer.classList.add('hidden');
-})
+clearBtn.addEventListener("click", function () {
+  input.value = "";
+  quoteContainer.classList.remove("hidden");
+  cardsContainer.classList.add("hidden");
+});
 
+// =============enter to find city weather===============
+
+input.addEventListener("keyup", function (e) {
+  const city = input.value;
+
+  if (e.key == "Enter" && input.value != "") {
+    getEnterWeather(city);
+  }
+});
+
+const getEnterWeather = async function (city) {
+  let apiKey = "bbf8d5e980c72026e64768d1c50ebe79";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  const data = await fetch(api);
+
+  let jsonData = await data.json();
+  console.log(jsonData);
+  weatherDetails(jsonData);
+
+  quoteContainer.classList.add('hidden');
+  cardsContainer.classList.remove('hidden');
+};
 // =========Get Device Location function=========
 
-let lat,lng;
+getDeviceLocation.addEventListener("click", function (position) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      let currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${"bbf8d5e980c72026e64768d1c50ebe79"}`;
 
-function getGeoLocation (position){
-    navigator.geolocation.getCurrentPosition((position) => {
-       lat = position.coords.latitude;
-       lng = position.coords.longitude;
-       console.log(lat,lng);
-   },() => {
-       alert("Could not get you location")
-   });
-}
-getDeviceLocation.addEventListener('click',getGeoLocation);
+      const getCurrentWeather = async function () {
+        const data = await fetch(currentWeather);
+
+        let jsonData = await data.json();
+        console.log(jsonData);
+
+        weatherDetails(jsonData);
+      };
+      getCurrentWeather();
+    },
+    () => {
+      alert("Could not get you location");
+    }
+  );
+});
 
 // ============= Search weather by city name functionality===========
 
-searchBtn.addEventListener('click',function(){
-    const city = input.value;
-    let apiKey = "bbf8d5e980c72026e64768d1c50ebe79";
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    
-    const getWeather = async function(){
-        const data = await fetch(api);
+async function weatherDetails(jsonData) {
+  const city = await jsonData.name;
+  const country = await jsonData.sys.country;
+  const id = await jsonData.weather[0].id;
+  // console.log(id);
+  //current temp area functionality
+  temp.textContent = `${jsonData.main.temp}\u00B0 C`;
+  description.textContent = jsonData.weather[0].description;
+  humid.textContent = `${jsonData.main.humidity}%`;
+  minMax.textContent = `${jsonData.main.temp_max} / ${jsonData.main.temp_min}`;
+  cityCountry.textContent = `${city}, ${country}`;
 
-        let jsonData = await data.json()
-        console.log(jsonData);
+  // ============icons according to weather===========
+  if (id === 800) {
+    weatherIcon.src = "src/img/clear.png";
+  }
 
-        const city =  await jsonData.name;
-        const country = await jsonData.sys.country;
-        const id = await jsonData.weather[0].id;
-        // console.log(id);
-        //current temp area functionality
-        temp.textContent = `${jsonData.main.temp}\u00B0 C`;
-        description.textContent = jsonData.weather[0].description;
-        humid.textContent = `${jsonData.main.humidity}%`;
-        minMax.textContent = `${jsonData.main.temp_max} / ${jsonData.main.temp_min}`;
-        cityCountry.textContent = `${city}, ${country}`;
+  if (id >= 200 && id <= 232) {
+    weatherIcon.src = "src/img/thunderstrom.png";
+  }
 
-        // ============icons according to weather===========
+  if (id >= 300 && id <= 321) {
+    weatherIcon.src = "src/img/drizzle.png";
+  }
 
+  if (id >= 500 && id <= 531) {
+    weatherIcon.src = "src/img/rain.png";
+  }
 
-    }
+  if (id >= 600 && id <= 622) {
+    weatherIcon.src = "src/img/snow.png";
+  }
 
-    getWeather();
-})
+  if (id >= 701 && id <= 781) {
+    weatherIcon.src = "src/img/atmosphere.png";
+  }
 
+  if (id >= 801 && id <= 804) {
+    weatherIcon.src = "src/img/cloud.png";
+  }
+  quoteContainer.classList.add('hidden');
+  cardsContainer.classList.remove('hidden');
+}
 
+searchBtn.addEventListener("click", function () {
+  const city = input.value;
+  let apiKey = "bbf8d5e980c72026e64768d1c50ebe79";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-    
+  const getWeather = async function () {
+    const data = await fetch(api);
+
+    let jsonData = await data.json();
+    console.log(jsonData);
+
+    weatherDetails(jsonData);
+  };
+  getWeather();
+});
